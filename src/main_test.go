a0be0c9e10fc93506cd7e1a33107c67d62adb79a
masebb <30523995+masebb@users.noreply.github.com>
2023-01-08 02:13:34 +0900

    テスト完成

--- src/main_test.go
+++ src/main_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -15,25 +17,38 @@ func TestPost(t *testing.T) {
 		wantStatus   int
 		wantResponse string
 	}{
-		{"通常リクエスト", "{ \"increment\": 1 }", http.StatusOK, "{\n\"like\":1\n}\n"},
-		{"いいねの数が多すぎるリクエスト", "{\"increment\": 100}", http.StatusBadRequest, ""},
-		{"アホリクエスト", "{\"ahomanuke\": \"a\"}", http.StatusBadRequest, ""},
+		//正常系
+		{"increment : 1", marshal(Request{1}), http.StatusOK, marshal(Response{1, ""})},
+		//異常系
+		{"incrementの数が多すぎるリクエスト", marshal(Request{100}), http.StatusBadRequest, marshal(Response{1, "increment is too large"})},
+		//異常系
+		{"アホリクエスト", `{"ahomanuke": "a"}`, http.StatusBadRequest, marshal(Response{1, "Bad Request"})},
+		//{"アホリクエスト", `{"like": "aho"}`, http.StatusBadRequest, marshal(Response{1, ""})},
 	}
+
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			e := echo.New()
+			//リクエストの作成
 			req := httptest.NewRequest(http.MethodPost, "/v1/like", strings.NewReader(test.requestBody))
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			//レコーダーの作成
 			rec := httptest.NewRecorder()
 			c := e.NewContext(req, rec)
 			if err := likeIncrement(c); err != nil {
 				t.Errorf("likeIncrement() error = %v", err)
 			}
-			if rec.Code != test.wantStatus {
-				t.Errorf("likeIncrement() status = %v, want %v", rec.Code, test.wantStatus)
-			}
-			if rec.Body.String() != test.wantResponse {
-				t.Errorf("likeIncrement() response = %v, want %v", rec.Body.String(), test.wantResponse)
-			}
+			assert.Equal(t, test.wantStatus, rec.Code)
+			assert.Equal(t, test.wantResponse, rec.Body.String())
 		})
 	}
+
+}
+func marshal(obj interface{}) string {
+	b, err := json.Marshal(obj)
+	if err != nil {
+		panic(err)
+	}
+	// https://github.com/labstack/echo/discussions/2024
+	return string(b) + "\n"
 }
